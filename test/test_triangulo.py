3c154d99cf3ad349495cd1cfa09a2fd811778678
Ricardo Brito <@britodfbr>
2017-07-16 09:43:09 -0300

    ajustes para nova classe

--- test/test_triangulo.py
+++ test/test_triangulo.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from incolumepy.geometria.triangulo import Triangulo
 
-class TestQuadrado(TestCase):
+class TestTriangulo(TestCase):
 
     def setUp(self):
         TestCase.setUp(self)
@@ -12,14 +12,16 @@ class TestQuadrado(TestCase):
         # de acordo com a formula de area do quadrado
         self.fig.base = 2
         self.fig.altura = 7.0
-        self.assertEqual(self.fig.get_area(), 14.)
+        self.assertEqual(self.fig.get_area(), 14.0)
 
         self.fig.base = 7
         self.fig.altura = 7.0
         self.assertEqual(self.fig.get_area(), 49.0)
 
     def test_get_perimetro(self):
-        self.fig.lados = [2,3,5]
-        self.assertEqual(self.fig.get_perimetro(), 10)
+        self.fig.lados = [2,5,5]
+        self.assertEqual(self.fig.get_perimetro(), 12.0)
+        self.fig.lados = [5, 5, 5]
+        self.assertEqual(self.fig.get_perimetro(), 15.0)
         self.fig.lados = [12, 15, 19]
         self.assertEqual(self.fig.get_perimetro(), 46.0)
